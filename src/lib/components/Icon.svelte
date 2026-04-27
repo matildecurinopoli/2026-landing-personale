411f2a36c19c93204d1181b49bc4a668c088f96b
matildecurinopoli <matilde.curino@mail.polimi.it>
2026-04-27 17:07:08 +0200

    add icon components

---
+++ src/lib/components/Icon.svelte
@@ -0,0 +1,33 @@
+<script>
+    const { name, variant = "regular"} = $props();
+
+    let pack = $derived.by(() => {
+        switch (variant) {
+            case "regular": 
+                return "ph";
+            case"fill":
+                return "ph-fill";
+            default: 
+                return "ph";
+        }
+    });
+</script>
+
+/* inserisco il link delle icone nell'head di questo documento e non 
+in +layout.svelte perché altrimenti tutte le pagine caricherebbero le icone anche quando
+non vengono usate */
+<svelte:head>
+    <link
+      rel="stylesheet"
+      type="text/css"
+      href="https://cdn.jsdelivr.net/npm/@phosphor-icons/web@2.1.1/src/regular/style.css"
+    />
+
+    <link
+      rel="stylesheet"
+      type="text/css"
+      href="https://cdn.jsdelivr.net/npm/@phosphor-icons/web@2.1.1/src/fill/style.css"
+    />
+</svelte:head>
+  
+<i class="{pack} ph-{name}"></i>
