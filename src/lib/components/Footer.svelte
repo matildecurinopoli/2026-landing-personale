f1a45f0b8b05275fbd8ccd64bd4f2248db69631e
matildecurinopoli <matilde.curino@mail.polimi.it>
2026-04-27 18:15:12 +0200

    header + footer
    non funziona

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,21 @@
+<script>
+	import letterboxd from "$lib/assets/letterboxd-logo-v-neg-rgb.svg";
+</script>
+
+<footer class="safe-area">
+	<img src={letterboxd} alt="Letterboxd" />
+</footer>
+
+<style>
+	footer {
+		padding-block: var(--size-4);
+
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	img {
+		height: var(--size-4);
+	}
+</style>
