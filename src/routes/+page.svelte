f1d654be8e090a14c054446bc99bf60cd150bdc9
matildecurinopoli <matilde.curino@mail.polimi.it>
2026-04-28 22:58:25 +0200

    test buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,25 @@
 <script>
 	import Hero from '$lib/components/Hero.svelte';
+    import Button from '$lib/components/Button.svelte';
 </script>
 
 <Hero />
+
+<div style="padding: var(--size-10) var(--size-11); display: flex; gap: var(--size-4); flex-wrap: wrap;">
+	
+	<Button 
+		variant="primary" 
+		href="https://letterboxd.com/" 
+		trailingIcon="arrow-right"
+	>
+		See on Letterboxd
+	</Button>
+
+	<Button 
+		variant="secondary" 
+		leadingIcon="play"
+	>
+		Watch trailer
+	</Button>
+
+</div>
