f62af826907eac125ed4e273ee1fe334589129dc
matildecurinopoli <matilde.curino@mail.polimi.it>
2026-04-29 10:04:42 +0200

    array movie info

---
+++ src/lib/data/movies-info.js
@@ -0,0 +1,51 @@
+import posterMarieAntoinette from '$lib/assets/movie-posters/Marie_Antoinette_movie poster.jpg';
+import posterPaterson from '$lib/assets/movie-posters/Paterson_movie poster.jpg';
+import posterSpencer from '$lib/assets/movie-posters/Spencer_movie poster.jpg';
+import posterLeCittàDiPianura from '$lib/assets/movie-posters/Le città di pianura_movie poster.jpg';
+
+export const myFourFavorites = [
+    {
+        //movie 1
+        movieName: "Marie Antoinette",
+        year: "2006",
+        director: "Sofia Coppola",
+        genre: "Drama, History",
+        country: "USA, France, Japan",
+        locandina: posterMarieAntoinette, 
+        letterboxdMovieUrl: "https://letterboxd.com/film/marie-antoinette-2006/",
+        trailerUrl: "https://youtu.be/yBWyKRoh98U?si=54yYZ6Lmq1M9GKHh"
+    },
+    {
+        //movie 2
+        movieName: "Paterson",
+        year: "2016",
+        director: "Jim Jarmusch",
+        genre: "Comedy, Drama, Romance",
+        country: "USA, Germany, France",
+        locandina: posterPaterson,
+        letterboxdMovieUrl: "https://letterboxd.com/film/paterson/",
+        trailerUrl: "https://youtu.be/E8_L9kqie7s?si=NAR8u5TbAVVOrk-2"
+    },
+    {
+        //movie 3
+        movieName: "Spencer",
+        year: "2021",
+        director: "Pablo Larraín",
+        genre: "Drama",
+        country: "Chile, Germany, UK, USA",
+        locandina: posterSpencer,
+        letterboxdMovieUrl: "https://letterboxd.com/film/spencer-2021/",
+        trailerUrl: "https://youtu.be/WllZh9aekDg?si=5FdFV7MaaDWs3Aw3"
+    },
+    {
+        //movie 4
+        movieName: "Le città di pianura",
+        year: "2025",
+        director: "Francesco Sossai",
+        genre: "Comedy",
+        country: "Italy, Germany",
+        locandina: posterLeCittàDiPianura,
+        letterboxdMovieUrl: "https://letterboxd.com/film/the-last-one-for-the-road/",
+        trailerUrl: "https://youtu.be/5kYHufR72P0?si=szP7Rlcgb-_xzHPY"
+    }
+];
